f8508cfb0a28b77905a2320a6f1b62060b0c9b74
korotas <korota-sasha@bk.ru>
2024-07-11 18:15:57 +0400

    add pickle serialization

--- deep_sort_realtime/deepsort_controller.py
+++ deep_sort_realtime/deepsort_controller.py
@@ -1,12 +1,14 @@
 import json
+import pickle
 
 from deep_sort_realtime.deepsort_tracker import DeepSort
 
 
 class DeepSortController:
-    def __init__(self, deepsort_kwargs, round_big_arrays_to=32):
+    def __init__(self, deepsort_kwargs, round_big_arrays_to=32, serialize='json'):
         self.deepsort_kwargs = deepsort_kwargs
         self.round_big_arrays_to = round_big_arrays_to
+        self.serialize = serialize
 
     def update(
         self,
@@ -25,11 +27,23 @@ class DeepSortController:
     def _create_tracker(self, state_path, create_new=False):
         if create_new:
             return DeepSort(**self.deepsort_kwargs)
-        with open(state_path, 'r') as r:
-            tracker_data = json.load(r)
-        return DeepSort.from_json(tracker_data)
+        if self.serialize == 'json':
+            with open(state_path, 'r') as r:
+                tracker_data = json.load(r)
+            return DeepSort.from_json(tracker_data)
+        elif self.serialize == 'pickle':
+            with open(state_path, 'rb') as r:
+                return pickle.load(r)
+        else:
+            raise NotImplementedError()
 
     def _save_tracker_data(self, state_path, tracker):
-        tracker_data = tracker.to_json(round_big_arrays_to=self.round_big_arrays_to)
-        with open(state_path, 'w+') as w:
-            w.write(json.dumps(tracker_data))
+        if self.serialize == 'json':
+            tracker_data = tracker.to_json(round_big_arrays_to=self.round_big_arrays_to)
+            with open(state_path, 'w+') as w:
+                w.write(json.dumps(tracker_data))
+        elif self.serialize == 'pickle':
+            with open(state_path, 'wb') as w:
+                pickle.dump(tracker, w)
+        else:
+            raise NotImplementedError()
